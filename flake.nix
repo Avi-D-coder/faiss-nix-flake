3dedebf47cc5658fc81c73c5287f15e5788d9e30
Avi Dessauer <avi.the.coder@gmail.com>
2023-03-08 17:14:55 -0600

    Add nix flake for rust bindings

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        # Common derivation arguments used for all builds
+        commonArgs = {
+          src = ./.;
+
+          buildInputs = with pkgs; [
+            # Add necessary build inputs here
+            cmake
+            llvmPackages.openmp
+            blas
+            swig
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            # Add necessary native build inputs here
+            cudaPackages.cudatoolkit
+            addOpenGLRunpath
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath commonArgs.buildInputs;
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        };
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "faiss-c-lib";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cmakeFlags = [
+            "-DFAISS_ENABLE_C_API=ON"
+            "-DBUILD_SHARED_LIBS=ON"
+            "-DFAISS_ENABLE_GPU=ON"
+            "-DCMAKE_BUILD_TYPE=Release"
+            "-DFAISS_ENABLE_PYTHON=OFF"
+          ];
+
+          buildInputs = commonArgs.buildInputs;
+          nativeBuildInputs = commonArgs.nativeBuildInputs;
+        };
+
+        devShells.default =
+          pkgs.mkShell (commonArgs // { });
+      }
+    );
+}
